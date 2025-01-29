c4e957e055c49b06db7f37aba57a424d5bb875a2
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-29 08:43:16 +0530

    Create s_text.py

---
+++ plugins/s_text.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import Config
+from globals import AuthU
+from Func.simples import mention_user
+
+@Client.on_message(filters.command("start"))
+async def st_rep(client,message:Message):
+    await message.reply(f"**🔰RVX M3U8 Downloader🔰\n\n❤️Welcome {mention_user(message)}💪!\n😎I am an simple M3U8 link uploader bot.\n😊Give me a m3u8 link with `/m3u8 <DirectLink>` and \n😇I will upload it to telegram as `video - mp4`😎**")
+
+@Client.on_message(filters.command("help"))
+async def st_help(client,message:Message):
+    await message.reply("🫠**No avalable!**\n\n**Commands: **\n\n  /start\n  /help\n  /checkauth\n  /addauth\n  /removeauth\n  /m3u8\n  /logo : use carefully")
+  
