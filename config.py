d365dede699f18eeb004b818050f1ab4f551405f
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-29 08:40:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config(object):
+  API_ID = int(os.getenv("apiid"))
+  
+  API_HASH = os.getenv("apihash")
+  
+  BOT_TOKEN = os.getenv("tk")
+  
+  AUTH = os.getenv("auth")
+  
+  OWNER =os.getenv("owner")
+
+  PW =int(os.getenv("spw"))
+
+  PRARAL_LIMIT = int(os.getenv("pps",3))
